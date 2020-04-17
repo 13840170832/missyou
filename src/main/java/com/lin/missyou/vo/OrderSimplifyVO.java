4482787c7dc378a502ecd8e85569d43b8c9133d7
13840170832 <328755509@qq.com>
2020-04-17 17:33:05 +0800

    返回给前端的订单信息

---
+++ src/main/java/com/lin/missyou/vo/OrderSimplifyVO.java
@@ -0,0 +1,24 @@
+package com.lin.missyou.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+public class OrderSimplifyVO {
+    private Long id;
+    private String orderNo;
+    private BigDecimal totalPrice;
+    private Long totalCount;
+    private String snapImg;
+    private String snapTitle;
+    private BigDecimal finalTotalPrice;
+    private Integer status;
+    private Date expiredTime;
+    private Date placeTime;
+    private Long period;
+
+}
