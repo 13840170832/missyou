46285effb736446717834c57011e38e68ad777f0
13840170832 <328755509@qq.com>
2020-04-26 10:01:36 +0800

    返回给前端的订单信息

--- src/main/java/com/lin/missyou/repository/SkuRepository.java
+++ src/main/java/com/lin/missyou/repository/SkuRepository.java
@@ -4,6 +4,7 @@ import com.lin.missyou.model.Sku;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -18,4 +19,8 @@ public interface SkuRepository extends JpaRepository<Sku,Long> {
             "and s.stock >= :quantity")
     int reduceStock(Long sid, Long quantity);
 
+
+    @Modifying
+    @Query("update Sku s set s.stock=s.stock+(:quantity) where s.id = :sid")
+    int recoverStock(@Param("sid") Long sid,@Param("quantity") Long quantity);
 }
