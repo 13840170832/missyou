5300a1871f4f2d603fe6ec7d64c6d9815f2c2fbb
13840170832 <328755509@qq.com>
2020-04-26 09:59:48 +0800

    返回给前端的订单信息

--- src/main/java/com/lin/missyou/api/v1/PaymentController.java
+++ src/main/java/com/lin/missyou/api/v1/PaymentController.java
@@ -1,5 +1,10 @@
 package com.lin.missyou.api.v1;
 
+import com.github.wxpay.sdk.LinWxPayConfig;
+import com.lin.missyou.core.LocalUser;
+import com.lin.missyou.core.interceptors.ScopeLevel;
+import com.lin.missyou.lib.LinWxNotify;
+import com.lin.missyou.service.WxPaymentNotifyService;
 import com.lin.missyou.service.WxPaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -11,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.constraints.Positive;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 @RequestMapping("/payment")
@@ -21,7 +28,11 @@ public class PaymentController {
     @Autowired
     private WxPaymentService wxPaymentService;
 
+    @Autowired
+    private WxPaymentNotifyService wxPaymentNotifyService;
+
     @PostMapping("/pay/order/{id}")
+    @ScopeLevel
     public Map<String,String> preWxOrder(@PathVariable(name="id") @Positive Long oid){
         Map<String,String> miniPayParams = wxPaymentService.preOrder(oid);
         return miniPayParams;
@@ -31,6 +42,20 @@ public class PaymentController {
     public String payCallback(HttpServletRequest request,
                               HttpServletResponse response){
 
-        return null;
+        InputStream s;
+        try {
+            s = request.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return LinWxNotify.fail();
+        }
+        String xml;
+        xml = LinWxNotify.readNotify(s);
+        try{
+            wxPaymentNotifyService.processPayNotify(xml);
+        }catch (Exception e){
+            return LinWxNotify.fail();
+        }
+        return LinWxNotify.success();
     }
 }
